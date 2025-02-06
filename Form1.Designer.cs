fbce68bda61f1941b4dbb4908f6ca6e96467a8a5
DangerousPixel <184161019+xDPixel@users.noreply.github.com>
2025-02-06 12:42:20 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace MyMaintenanceApp
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Initialize all components for the form.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "MercClean - GUI";
+            this.ResumeLayout(false);
+        }
+    }
+}
