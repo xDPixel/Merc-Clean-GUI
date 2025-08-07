4787914d4b5fab220fa25720410ab2eddded3440
xDPixel <xdpixel@example.com>
2025-08-07 17:50:55 +0300

    Add checkbox for browser login data clearing with warning popup

---
+++ Form1.cs
@@ -0,0 +1,11 @@
+using System.Windows.Forms;
+namespace MyMaintenanceApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
